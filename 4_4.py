bc7942e53634032f870fdca50a58ea00a254ff82
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-12-23 16:17:49 +0700

    Initial commit

---
+++ 4_4.py
@@ -0,0 +1,72 @@
+import numpy
+import random
+
+
+MATERIAL_COST = 20
+WORK_COST = 5
+POLISH_COST = 4
+FIX_FAT_COST = 3
+POLISH_FIX_COST = 2
+
+NORM_WORK_P = 0.8
+BROKEN_WORK_P = 0.08
+FAT_WORK_P = 0.12
+
+POLISH_UP_MISTAKE_P = 0.03
+POLISH_DOWN_MISTAKE_P = 0.06
+
+DETAIL_PROFIT = 35
+
+
+def simulate_one() -> float:
+    # 1. Токарка
+    cost = MATERIAL_COST + WORK_COST 
+    work_p = random.random()
+
+    if work_p < BROKEN_WORK_P:
+        return -cost
+    elif work_p < FAT_WORK_P:
+        cost += FIX_FAT_COST
+    
+    # 2. Шлифовка
+    cost += POLISH_COST
+    polish_mistake_p = random.random()
+    
+    # сразу две сломалось -> произведение вероятностей
+    if(polish_mistake_p < POLISH_DOWN_MISTAKE_P * POLISH_UP_MISTAKE_P):
+        return -cost
+    
+    # возникновение одного или другого -> сумма вероятностей
+    elif(polish_mistake_p < POLISH_UP_MISTAKE_P + POLISH_DOWN_MISTAKE_P):
+        cost += POLISH_FIX_COST
+
+    return DETAIL_PROFIT - cost
+
+
+def check_for(n_runs):
+    good_count = 0
+    total_profit = 0.0
+    is_debug_on = False
+
+    for _ in range(n_runs):
+        profit = simulate_one()
+        if profit > 0:
+            good_count += 1
+        total_profit += profit
+
+    good_prob = good_count / n_runs
+    avg_profit = total_profit / n_runs
+    print(f"--- Результаты за {n_runs} испытаний ---")
+    print(f"Вероятность годной детали: {good_prob:.4f}")
+    print(f"Средняя прибыль на деталь: {avg_profit:.4f}")
+    print()
+
+
+if __name__ == "__main__":
+    check_for(10)
+    check_for(100)
+    check_for(1000)
+    check_for(10000)
+    check_for(100_000)
+    check_for(1_000_000)
+    
